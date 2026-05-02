7483a7303af65471df827f2b27765c4c5fff176c
super12pl <szymon.grela2006@gmail.com>
2026-05-02 23:22:24 +0200

    setup

---
+++ legacy2e.mjs
@@ -0,0 +1,37 @@
+/**
+Legacy 2E system for Foundry VTT
+Author: super12pl
+*/
+console.log("    ⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ \n⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ \n⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ \n⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ \n⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ \n⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ \n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \n⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ \n⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ \n⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀\n ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+// import { InvisibleSunVislaeActor } from "./module/actor/vislae.js";
+// import { InvisibleSunVislaeActorSheet } from "./module/actor/vislae-sheet.js";
+// import { InvisibleSunItem } from "./module/item/item.js";
+// import { InvisibleSunItemSheet } from "./module/item/item-sheet.js";
+$("head").append($("<script type = 'text/javascript'></script>").attr("src", 'https://code.jquery.com/jquery-3.7.1.js'));
+$("head").append($("<script type = 'text/javascript'></script>").attr("src", 'https://code.jquery.com/ui/1.14.2/jquery-ui.js'));
+
+Hooks.once("init", async function () {
+    //base classes
+    // CONFIG.Actor.documentClass = InvisibleSunVislaeActor
+    // CONFIG.Item.documentClass = InvisibleSunItem
+
+    // //sheets
+    // Actors.unregisterSheet("core", ActorSheet)
+    // Actors.registerSheet("invisiblesun", InvisibleSunVislaeActorSheet, {
+    //     makeDefault: true,
+    //     label: "invisiblesun.Sheet.Actor"
+    // })
+
+    // Items.unregisterSheet("core", ItemSheet)
+    // Items.registerSheet("invisiblesun", InvisibleSunItemSheet, {
+    //     makeDefault: true,
+    //     label: "invisiblesun.Sheet.Item"
+    // })
+
+    //trackable attributes (on tokens)
+    // CONFIG.Actor.trackableAttributes = {
+    //     vislae: {
+    //         value: ["combat.damage.injuries", "combat.damage.anguish", "combat.damage.wounds"]
+    //     }
+    // }
+});
